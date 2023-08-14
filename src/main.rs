38918d329cc9b653bb4dfd40d9b6d26f9dc62a40
Alex. Z <azavgo@yahoo.com>
2023-08-14 20:49:54 +1000

    Address the unused import warning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 //ASCI symbols selected for the password generation: 33 = "!", 35 = "#" - 38 = "&", 40 = "(" - 93 = "]", 95 = "_", 97 = "a" - 125 = "}"
-//The array is 0..88. rng.gen_range(0..89) generates a number [0..89), 89 is not included, so it is [0..88]
+//rng.gen_range(0..89) is equivalent to [0..89), 89 is not included, so it is the same as [0..88]
 
 use rand::Rng;
-use std::string::{String, FromUtf8Error};
-use std::env::{self, args};
-use std::num::ParseIntError; 
+use std::env;
+use std::num::ParseIntError;
+use std::string::{FromUtf8Error, String};
 
 #[derive(Debug)]
 pub enum PassgenError {
@@ -24,24 +24,30 @@ impl From<ParseIntError> for PassgenError {
     }
 }
 
-fn passgen(n: u8) -> Result<String, PassgenError>{
-    let a:[u8; 89] = [33, 35, 36, 37, 38, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 95, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125];
-    
+fn passgen(n: u8) -> Result<String, PassgenError> {
+    let a: [u8; 89] = [
+        33, 35, 36, 37, 38, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+        58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+        81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 95, 97, 98, 99, 100, 101, 102, 103,
+        104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+        122, 123, 124, 125,
+    ];
+
     let mut rng = rand::thread_rng();
-    let mut pass_vec: Vec<u8> = vec![0; 8]; 
-    
+    let mut pass_vec: Vec<u8> = vec![0; 8];
+
     for _i in 0..n {
         pass_vec.push(a[rng.gen_range(0..89)]);
     }
-    
+
     let password = String::from_utf8(pass_vec)?;
-    
+
     Ok(password)
 }
 
 pub fn passgen_ui() -> Result<(), PassgenError> {
     let args: Vec<String> = env::args().collect();
-    
+
     match args.len() > 1 {
         true => {
             if args[1] == "?" || args[1] == "--help" {
@@ -50,19 +56,16 @@ pub fn passgen_ui() -> Result<(), PassgenError> {
                 let n = args[1].parse::<u8>()?;
                 println!("Password {} characters length: {}", n, passgen(n)?);
             }
-            
-            
-        }, 
-        _    => {
-            println!("Password 8 characters length (default): {}", passgen(8)?);    
+        }
+        _ => {
+            println!("Password 8 characters length (default): {}", passgen(8)?);
         }
     }
     Ok(())
 }
 
 fn main() -> Result<(), PassgenError> {
-    
     passgen_ui()?;
-     
+
     Ok(())
 }
