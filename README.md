4903814e84776fe07de2ffd6aff57ef735988856
Alex. Z <azavgo@yahoo.com>
2023-08-14 20:38:15 +1000

    Added help arguments

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 ## Features: 
 1. Generates pseudo-random passwords; 
 1. Password length is between 1 and 256 ASCI characters, default is 8 characters; 
-1. ASCI characters selected for the password generation: 33 = "!", 35 = "#" to 38 = "&", 40 = "(" to 93 = "]", 97 = "a" to 125 = "}". All other ASCI characters are excluded, e.g. quotation marks, grave accent, caret etc. 
+1. ASCI characters selected for the password generation: 33 = "!", 35 = "#" to 38 = "&", 40 = "(" to 93 = "]", 95 = "_", 97 = "a" to 125 = "}". All other ASCI characters are excluded, e.g. quotation marks, grave accent, caret etc. 
 
 ## How to use passgen utility
-1. Type passgen in a terminal and press Enter to generate a default 8-character password: 
+1. Type ./passgen in a terminal and press Enter to generate a default 8-character password: 
 ```
-$ passgen
+$ ./passgen
 $ Password 8 characters length (default): iSPW}y\1
 ```
-1. In a terminal type passgen and add a length of the password, e.g a 12-character password: 
+1. In a terminal type ./passgen and add a length of the password, e.g a 12-character password: 
 ```
-$ passgen 12
+$ ./passgen 12
 $ Password 12 characters length: e>C:kl|2&xt7
+```
+
+1. For help, type ./passgen ?, alternatively type ./passgen --help in a terminal: 
+```
+$ ./passgen ?
+$ usage: passgen [? | --help] [n=<number between 0 and 255, length of the password>]
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-//ASCI symbols selected for the password generation: 33 = "!", 35 = "#" - 38 = "&", 40 = "(" - 93 = "]", 97 = "a" - 125 = "}"
-//The array is 0..87. rng.gen_range(0..88) generates a number [0..88), 88 is not included, so it is [0..87]
+//ASCI symbols selected for the password generation: 33 = "!", 35 = "#" - 38 = "&", 40 = "(" - 93 = "]", 95 = "_", 97 = "a" - 125 = "}"
+//The array is 0..88. rng.gen_range(0..89) generates a number [0..89), 89 is not included, so it is [0..88]
 
 use rand::Rng;
 use std::string::{String, FromUtf8Error};
-use std::env;
+use std::env::{self, args};
 use std::num::ParseIntError; 
 
 #[derive(Debug)]
@@ -25,13 +25,13 @@ impl From<ParseIntError> for PassgenError {
 }
 
 fn passgen(n: u8) -> Result<String, PassgenError>{
-    let a:[u8; 88] = [33, 35, 36, 37, 38, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125];
+    let a:[u8; 89] = [33, 35, 36, 37, 38, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 95, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125];
     
     let mut rng = rand::thread_rng();
     let mut pass_vec: Vec<u8> = vec![0; 8]; 
     
     for _i in 0..n {
-        pass_vec.push(a[rng.gen_range(0..88)]);
+        pass_vec.push(a[rng.gen_range(0..89)]);
     }
     
     let password = String::from_utf8(pass_vec)?;
@@ -44,8 +44,14 @@ pub fn passgen_ui() -> Result<(), PassgenError> {
     
     match args.len() > 1 {
         true => {
-            let n = args[1].parse::<u8>()?;
-            println!("Password {} characters length: {}", n, passgen(n)?);
+            if args[1] == "?" || args[1] == "--help" {
+                println!("usage: passgen [? | --help] [n=<number between 0 and 255, length of the password>]");
+            } else {
+                let n = args[1].parse::<u8>()?;
+                println!("Password {} characters length: {}", n, passgen(n)?);
+            }
+            
+            
         }, 
         _    => {
             println!("Password 8 characters length (default): {}", passgen(8)?);    
